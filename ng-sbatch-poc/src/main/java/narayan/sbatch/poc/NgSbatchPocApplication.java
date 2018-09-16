6cf0de266758103686d9cd43fb760ecbc92ed3f9
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-09-16 15:48:25 +0530

    initial commit

---
+++ ng-sbatch-poc/src/main/java/narayan/sbatch/poc/NgSbatchPocApplication.java
@@ -0,0 +1,12 @@
+package narayan.sbatch.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSbatchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSbatchPocApplication.class, args);
+	}
+}
---
+++ ng-sbatch-poc/src/test/java/narayan/sbatch/poc/NgSbatchPocApplicationTests.java
@@ -0,0 +1,16 @@
+package narayan.sbatch.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NgSbatchPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
